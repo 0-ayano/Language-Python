9d4e0b566b96efa496e53f607b9e4394bb3fc192
Tkoya <86472869+0-ayano@users.noreply.github.com>
2022-11-22 10:06:40 +0900

    Update DisplayCamera.py

--- DisplayCamera.py
+++ DisplayCamera.py
@@ -1,8 +1,8 @@
+import os
+os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 import cv2
-import datetime
 
 def check_camera_connection():
-    print('[', datetime.datetime.now(), ']', 'searching any camera...')
     true_camera_is = []  # 空の配列を用意
 
     for camera_number in range(0, 10):
@@ -12,32 +12,26 @@ def check_camera_connection():
         if ret is True:
             true_camera_is.append(camera_number)
             print("camera_number", camera_number, "Find!")
-
-        else:
-            print("camera_number", camera_number, "None")
     return len(true_camera_is)
  
-camera = cv2.VideoCapture(0)                # カメラCh.(ここでは0)を指定
 nowCamera = 0
 maxCamera = check_camera_connection()
 box = -1
- 
-# 撮影＝ループ中にフレームを1枚ずつ取得（qキーで撮影終了）
+
+camera = cv2.VideoCapture(0) 
 while True:
-    ret, frame = camera.read()              # フレームを取得
-    cv2.imshow('camera', frame)             # フレームを画面に表示
+    ret, frame = camera.read()
+    cv2.imshow('camera', frame)
 
     if box != nowCamera:
-        print("Now Camera :", nowCamera)
+        print(nowCamera)
         box = nowCamera
  
     key = cv2.waitKey(100)
-    print("             :", key)
-    # キー操作があればwhileループを抜ける
+
     if key == ord('d'):
         print("     STOP")
         break
-
     elif key == ord('s'):
         print("     >>")
         if nowCamera == maxCamera-1:
@@ -54,13 +48,8 @@ while True:
             nowCamera = nowCamera - 1
         camera = cv2.VideoCapture(nowCamera)
         
-
-    
- 
-# 撮影用オブジェクトとウィンドウの解放
 camera.release()
 cv2.destroyAllWindows()
-
 
 """
 - [OpenCVで使われるwaitkeyとは?定義から実用例をわかりやすく解説!?](https://kuroro.blog/python/8DIolh7Pwggq2pvabysn/)
